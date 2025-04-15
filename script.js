d5bf2f642bb9335c5579b8807f5a5f3e0ac7f45e
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-04-15 21:20:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const textArea = document.getElementById('evaluatedText');
+const letters = document.getElementById('letterCount');
+
+let count = 0;
+
+textArea.addEventListener('input', ()=>{
+	count++;
+	letters.textContent = count;
+})
+
